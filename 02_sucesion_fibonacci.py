db35633aa1c8dc22cc8818434feda51165f2340b
PabloChorda <pablochorda365@gmail.com>
2024-10-15 00:05:04 +0200

    Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0

---
+++ 02_sucesion_fibonacci.py
@@ -0,0 +1,18 @@
+"""
+RETO #2
+
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+  la que el siguiente siempre es la suma de los dos anteriores.
+  0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+fibonacci_50 = list(fibonacci(50))
+print(fibonacci_50)
