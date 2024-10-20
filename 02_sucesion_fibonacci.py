038a954444cc8310e111869ceee6176a25b6ca39
PabloChorda <pablochorda365@gmail.com>
2024-10-20 18:16:04 +0200

    Ejercicio 02

--- 02_sucesion_fibonacci.py
+++ 02_sucesion_fibonacci.py
@@ -1,11 +1,11 @@
 """
-RETO #2
 
 Escribe un programa que imprima los 50 primeros números de la sucesión
-de Fibonacci empezando en 0.
+de  Fibonacci empezando desde 0.
 - La serie Fibonacci se compone por una sucesión de números en
   la que el siguiente siempre es la suma de los dos anteriores.
   0, 1, 1, 2, 3, 5, 8, 13...
+
 """
 
 def fibonacci(n):
