300f2e28a2de2a72117e91a13ba56f23e116996a
PabloChorda <pablochorda365@gmail.com>
2024-11-14 22:37:43 +0100

    Ejercicio 12

--- 12_codigo_morse.py
+++ 12_codigo_morse.py
@@ -5,7 +5,7 @@ morse y viceversa.
   la conversión.
 - En morse se soporta raya "—", punto ".", un espacio " " entre letras
   o símbolos y dos espacios entre palabras "  ".
-- El alfabeto morse soportado será el mostrado en
+- El alfabeto morse que soportará será el mostrado en
   https://es.wikipedia.org/wiki/Código_morse.
 """
 
