27160ca53b393d289eb76de554054dc425293082
PabloChorda <pablochorda365@gmail.com>
2024-11-24 22:22:36 +0100

    contraselas

---
+++ 15_verifica_contraseñas.py
@@ -0,0 +1,12 @@
+'''
+
+Crea un programa que verifique si una contraseña cumple con los 
+siguientes requisitos:
+
+La contraseña debe tener al menos 8 caracteres.
+Debe contener al menos una letra minúscula, una letra mayúscula, 
+un número y un carácter especial (como @, #, $, etc.).
+No debe contener espacios.
+El programa debe permitir al usuario ingresar una contraseña y verificar 
+si es válida o no. Si no es válida, deberá indicar qué requisitos no cumple.
+'''
