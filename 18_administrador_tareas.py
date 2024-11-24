75a45d3ae336c45f0cff6622e1201cc1710d6a01
PabloChorda <pablochorda365@gmail.com>
2024-11-24 22:41:41 +0100

    Ejercicio 18

---
+++ 18_administrador_tareas.py
@@ -0,0 +1,11 @@
+'''
+Crea un programa que funcione como un administrador básico de tareas.
+
+El programa debe permitir al usuario:
+Agregar tareas, indicando el título, descripción y prioridad (alta, media, baja).
+Listar todas las tareas con su información (incluyendo un estado: "Pendiente" o "Completada").
+Marcar una tarea como completada seleccionándola por su índice en la lista.
+Eliminar tareas completadas de la lista.
+El programa debe manejar entradas inválidas y mostrar menús interactivos para cada opción.
+'''
+
