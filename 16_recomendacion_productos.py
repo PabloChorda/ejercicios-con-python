6c8811f858bd19f11713c76a11ce3b3732ac2b25
PabloChorda <pablochorda365@gmail.com>
2024-11-24 22:28:52 +0100

    nuevo

---
+++ 16_recomendacion_productos.py
@@ -0,0 +1,10 @@
+'''
+Crea un programa que funcione como un sistema básico de recomendación de productos.
+
+El programa debe tener una lista de productos con descripciones.
+El usuario debe ingresar una palabra clave.
+El programa buscará la palabra clave en las descripciones de los productos
+y mostrará las recomendaciones.
+Si no encuentra coincidencias, mostrará un mensaje indicando que no hay recomendaciones 
+disponibles.
+'''
