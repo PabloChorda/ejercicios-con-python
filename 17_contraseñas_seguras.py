d1bdebafa1a54d470ef4d57051edae121b0702c1
PabloChorda <pablochorda365@gmail.com>
2024-11-24 22:32:38 +0100

    nou

---
+++ 17_contraseñas_seguras.py
@@ -0,0 +1,14 @@
+'''
+Crea un programa que genere contraseñas seguras de acuerdo con las preferencias del usuario.
+
+El programa debe permitir al usuario elegir:
+La longitud de la contraseña (mínimo 8 caracteres, máximo 64).
+Si incluirá letras mayúsculas.
+Si incluirá números.
+Si incluirá caracteres especiales.
+El programa debe generar y mostrar una contraseña aleatoria que cumpla 
+con los criterios seleccionados.
+Si el usuario selecciona criterios insuficientes (por ejemplo, no incluir 
+ningún tipo de carácter), debe mostrar un mensaje de error.
+'''
+
